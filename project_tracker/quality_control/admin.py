b8f2c5101f8f3af909495b7228f9aa5925f50b0d
solnyyysko <logos.vera@mail.ru>
2024-04-26 15:56:52 +0300

    Admin

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'priority', 'status', 'created_at', 'updated_at')
+    list_filter = ('project', 'priority', 'status')
+    search_fields = ('title', 'description')
+    list_editable = ('status',)
+    readonly_fields = ('project', 'task', 'created_at', 'updated_at')
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'priority', 'status', 'created_at', 'updated_at')
+    list_filter = ('project', 'priority', 'status')
+    search_fields = ('title', 'description')
+    list_editable = ('status',)
+    readonly_fields = ('project', 'task', 'created_at', 'updated_at')
